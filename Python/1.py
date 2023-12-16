398261d645ad9ba5e60ee3c772b223d197317381
MrSingh710 <js2244668800@gmail.com>
2023-12-16 12:52:28 +0530

    edits

--- Python/1.py
+++ Python/1.py
@@ -1,13 +1,21 @@
+# printing the strings and numbers
 print("welcome")
-#variable
+print('welcome')
+print('''welcome''')
+print(5*10/2)
+print('\n')
+
+# variable can store any data type 
 name = 'Jas'
+print(type(name))
 print('My name is ', name)
-print(name, 'likes nothing')
-print(name+ ' likes everything')
+print(name, 'likes nothing') # comma automatically adds a space
+print(name+ ' likes everything') # concatenation doesn't adds a space
+print(f'{name} likes something') # f-string method
 age = 20
+print(type(age))
 print('He is', age, 'years old')
-print('He is '+ str(age) + ' years old')
-#input 
+print('He is '+ str(age) + ' years old') # only strings can concatenate, so we have first typecasted the integer into string
 
 
 
---
+++ Python/2_input.py
@@ -0,0 +1,2 @@
+name = input('Enter name:\n') # can use \n for the cursor to be moved in the next line
+print(name)
